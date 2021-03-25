b13c1195682b2237ee87061708fd414e8335d565
Evan Barry <evan.barry@students.ittralee.ie>
2021-03-25 12:24:45 +0000

    Incorporated AdMob
    Also randomized which set of ads play (AdMob or Unity, 50/50 chance)

--- adsAssignment/Assets/unityAdsScripts/InitializeAdsScript.cs
+++ adsAssignment/Assets/unityAdsScripts/InitializeAdsScript.cs
@@ -3,25 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 using UnityEngine.UI;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
 
 [RequireComponent (typeof (Button))]
 public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
 {
     #if UNITY_IOS
+        //Unity Variables
         private string gameId = "4060649";
         private string mySurfacingIdRewarded = "Rewarded_iOS";
         private string mySurfacingIdBanner = "Banner_iOS";
+        private string mySurfacingIdInterstitial = "Interstitial_iOS";
+
+        //Google Variables
+        string appId = "ca-app-pub-6074173583459415~2674239925";
+        string bannerAdUnitId = "ca-app-pub-6074173583459415/7944055501";
+        string interstitialAdUnitId = "ca-app-pub-6074173583459415/6220087905";
+        string rewrdedAdUnitId = "ca-app-pub-6074173583459415/4993075644";
+
     #elif UNITY_ANDROID
+        //Unity Variables
         private string gameId = "4060648";
         private string mySurfacingIdRewarded = "Rewarded_Android";
         private string mySurfacingIdBanner = "Banner_Android";
+        private string mySurfacingIdInterstitial = "Interstitial_Android";
+
+        //Google Variables
+        string appId = "ca-app-pub-6074173583459415~3681566812";
+        string bannerAdUnitId = "ca-app-pub-6074173583459415/9670687255";
+        string interstitialAdUnitId = "ca-app-pub-6074173583459415/1792197233";
+        string rewrdedAdUnitId = "ca-app-pub-6074173583459415/7974354402";
     #endif
-    //string mySurfacingId = "Rewarded_iOS";
+
+    //Unity Variables
     bool testMode = true;
     bool iAdShown = false;
-    bool rAdShown = false;
-
+    bool startTimer = false;
+    public float countdownTime = 5f;
     Button myButton;
+
+    //Google Variables
+    private BannerView googleBannerView;
+    private InterstitialAd googleInterstitial;
+    private RewardedAd googleRewarded;
 
     // Start is called before the first frame update
     void Start()
@@ -30,30 +55,82 @@ public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
 
         if(myButton) 
         {
-            myButton.onClick.AddListener(showRewardedVideo);
+            if(Random.Range(0f,1f) <= 0.5f)
+            {
+                //Unity Rewarded
+                myButton.onClick.AddListener(showRewardedVideo);
+                Debug.Log("Unity Rewarded");
+            }
+
+            else
+            {
+                //Google Rewarded
+                Debug.Log("Google Rewarded");
+                myButton.onClick.AddListener(showGoogleRewardedVideo);
+            }
         }
 
         Advertisement.AddListener(this);
         Advertisement.Initialize(gameId, testMode);
-        StartCoroutine(showBannerWhenInitialized());
+
+        MobileAds.Initialize(initStatus => { });
+        
+        if(Random.Range(0f,1f) <= 0.5f)
+        {
+            //Unity Banner
+            Debug.Log("Unity Banner");
+            StartCoroutine(showBannerWhenInitialized());
+        }
+
+        else
+        {
+            //Google Banner
+            Debug.Log("Google Banner");
+            this.requestBanner();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        myButton.interactable = Advertisement.IsReady(mySurfacingIdRewarded);
-
         if(!iAdShown)
         {
-            showInterstitialAd();
+            if(Random.Range(0f,1f) <= 0.5f)
+            {
+                //Unity Interstitial
+                showInterstitialAd();
+                Debug.Log("Unity Interstitial");
+            }
+
+            else
+            {
+                //Google Interstitial
+                requestInterstitial();
+                if(this.googleInterstitial.IsLoaded())
+                {
+                    this.googleInterstitial.Show();
+                    countdownTime = 5f;
+                    startTimer = true;
+                    Time.timeScale = 1f;
+                    Debug.Log("Google Interstitial");
+                }
+                
+            }
         }
 
-        if(!rAdShown)
+        if(startTimer && countdownTime >= 0.0f)
         {
-            //showRewardedVideo();
+            countdownTime -= Time.deltaTime;
+        }
+
+        if(this.googleInterstitial != null && this.googleInterstitial.IsLoaded() && countdownTime <= 0.0f)
+        {
+            googleInterstitial.Destroy();
+            startTimer = false;
         }
     }
 
+    //Unity Banner
     IEnumerator showBannerWhenInitialized()
     {
         while(!Advertisement.isInitialized)
@@ -64,11 +141,35 @@ public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
         Advertisement.Banner.Show(mySurfacingIdBanner);
     }
 
+    //Google Banner
+    private void requestBanner()
+    {
+        // Create a 320x50 banner at the top of the screen.
+        this.googleBannerView = new BannerView(bannerAdUnitId, AdSize.SmartBanner, AdPosition.Bottom);
+
+        // Called when an ad request has successfully loaded.
+        this.googleBannerView.OnAdLoaded += this.HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.googleBannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        this.googleBannerView.OnAdOpening += this.HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        this.googleBannerView.OnAdClosed += this.HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.googleBannerView.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        this.googleBannerView.LoadAd(request);
+    }
+
+    //Unity Interstitial
     public void showInterstitialAd()
     {
-        if(Advertisement.IsReady())
+        if(Advertisement.IsReady(mySurfacingIdInterstitial))
         {
             Advertisement.Show();
+            //Debug.Log("Unity Interstitial");
             iAdShown = true;
         }
         
@@ -78,12 +179,26 @@ public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
         }
     }
 
+    //Google Interstitial
+    private void requestInterstitial()
+    {
+        this.googleInterstitial = new InterstitialAd(interstitialAdUnitId);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        this.googleInterstitial.LoadAd(request);
+        
+        iAdShown = true;
+    }
+
+    //Unity Rewarded
     public void showRewardedVideo()
     {
         if(Advertisement.IsReady(mySurfacingIdRewarded))
         {
+            countdownTime = 5f;
             Advertisement.Show(mySurfacingIdRewarded);
-            rAdShown = true;
+            Debug.Log("Unity Rewarded");
         }
 
         else
@@ -92,24 +207,63 @@ public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
         }
     }
 
+    //Google Rewarded
+    private void requestRewarded()
+    {
+        this.googleRewarded = new RewardedAd(rewrdedAdUnitId);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Called when the user should be rewarded for interacting with the ad.
+        this.googleRewarded.OnUserEarnedReward += HandleUserEarnedReward;
+
+        this.googleRewarded.LoadAd(request);
+    }
+
+    private void showGoogleRewardedVideo()
+    {
+        requestRewarded();
+        if(this.googleRewarded.IsLoaded())
+        {
+            this.googleRewarded.Show();
+        }
+    }
+
+    //Unity event handlers
     public void OnUnityAdsDidFinish (string surfacingId, ShowResult showResult) 
     {
         // Define conditional logic for each ad completion status:
-        if (showResult == ShowResult.Finished && surfacingId == mySurfacingIdRewarded) 
+        if (showResult == ShowResult.Finished) 
         {
-            Debug.Log("You finished the ads! Here is your reward!");
+            //Shows when close button pressed for interstitial and rewarded ads
+            //Debug.Log("You finished the ads! Here is your reward!");
+            if(surfacingId == mySurfacingIdRewarded)
+            {
+                if(countdownTime > 0f)
+                {
+                    Debug.Log("Unity - You closed the ad before a reward could be given - " + countdownTime + " seconds left");
+                }
+
+                else
+                {
+                    Debug.Log("Unity - You finished the ads! Here is your reward!");
+                    startTimer = false;
+                }
+            }
+
+            else if(surfacingId == mySurfacingIdInterstitial)
+            {
+                Debug.Log("Unity - You closed the ad");
+            }
         }
-        else if (showResult == ShowResult.Finished)
-        {
-            Debug.Log("You finished the ad");
-        } 
         else if (showResult == ShowResult.Skipped) 
         {
-            Debug.Log("You skipped the ad. No reward for you");
+            //Shows when skip button pressed for interstitial ad
+            Debug.Log("Unity - You skipped the ad. No reward for you");
         } 
         else if (showResult == ShowResult.Failed) 
         {
-            Debug.Log("The ad did not finish due to an error.");
+            Debug.Log("Unity - The ad did not finish due to an error.");
         }
     }
 
@@ -118,23 +272,64 @@ public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
         // If the ready Ad Unit or legacy Placement is rewarded, show the ad:
         if (surfacingId == mySurfacingIdRewarded || surfacingId == mySurfacingIdBanner) {
             // Optional actions to take when theAd Unit or legacy Placement becomes ready (for example, enable the rewarded ads button)
+            myButton.interactable = Advertisement.IsReady(mySurfacingIdRewarded);
         }
     }
 
     public void OnUnityAdsDidError (string message) 
     {
         // Log the error.
+        Debug.Log("Unity - Ad Error - " + message);
     }
 
     public void OnUnityAdsDidStart (string surfacingId) 
     {
         // Optional actions to take when the end-users triggers an ad.
+        if(surfacingId == mySurfacingIdRewarded)
+        {
+            startTimer = true;
+        }
     } 
 
-    // When the object that subscribes to ad events is destroyed, remove the listener:
+    // Google event handlers
     public void OnDestroy() 
     {
         Advertisement.RemoveListener(this);
+    }
+
+    public void HandleOnAdLoaded(object sender, System.EventArgs args)
+    {
+        MonoBehaviour.print("Google - HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("Google - HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnAdOpened(object sender, System.EventArgs args)
+    {
+        MonoBehaviour.print("Google - HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, System.EventArgs args)
+    {
+        MonoBehaviour.print("Google - HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, System.EventArgs args)
+    {
+        MonoBehaviour.print("Google - HandleAdLeavingApplication event received");
+    }
+
+    public void HandleUserEarnedReward(object sender, Reward args)
+    {
+        string type = args.Type;
+        double amount = args.Amount;
+        MonoBehaviour.print(
+            "HandleRewardedAdRewarded event received for "
+                        + amount.ToString() + " " + type);
     }
 
 }
